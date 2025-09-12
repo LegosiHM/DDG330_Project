7a085606f0c11dbcfb6735eb9e8d4bb571d0f17f
Goghocutter <veerapaj9@gmail.com>
2025-09-12 16:03:38 +0700

    Slime Scale Change

--- Assets/Scripts/SlimeManager.cs
+++ Assets/Scripts/SlimeManager.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 
 public class SlimeManager : MonoBehaviour
 {
-    [SerializeField] private float slimeDeathTimer = 10f;
+    [SerializeField] private float slimeDeathMaxTimer = 5f;
+    private float slimeDeathTimeLeft;
     private bool slimeStopMoving = false;
+    private Vector3 normalScale;
 
     void Start()
     {
-        
+        normalScale = transform.localScale;
+        slimeDeathTimeLeft = slimeDeathMaxTimer;
     }
 
     void Update()
     {
-        if (slimeDeathTimer >= 0)
+        if (slimeDeathTimeLeft >= 0)
         {
-            slimeDeathTimer -= Time.deltaTime;
+            slimeDeathTimeLeft -= Time.deltaTime;
+            normalScale.y += Time.deltaTime;
+            if (normalScale.y <= slimeDeathMaxTimer)
+            {
+                transform.localScale = normalScale;
+            }
+            else
+            {
+                normalScale.y = slimeDeathMaxTimer;
+            }
         }
         else
         {
             slimeStopMoving = true;
-            Debug.Log("Ability will go here");
+            //Debug.Log("Ability will go here");
             enabled = false;
         }
 
